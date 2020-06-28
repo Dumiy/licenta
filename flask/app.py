854fd8ee7e4ba0f200641fea2e761fe256262b90
Dumiy <45998406+Dumiy@users.noreply.github.com>
2020-06-28 20:25:28 +0300

    Added upload

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,33 @@
 import sys
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 import pandas as pd
+import os
 import numpy as np
+
+app.config['UPLOAD_FOLDER']='.\upload'
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
 app = Flask(__name__)
 
-@app.route('/hello')
+
+@app.route('/hello',methods=['POST'])
 def hello():
     return 'Hello, World'
 
 
 if __name__ == "__main__":
+    os.mkdir("\upload")
     try:
         port = int(sys.argv[1])
     except Exception as e:
