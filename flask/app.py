4491b804225c7895b9ca1def07ab809c2a826f5a
Dumiy <45998406+Dumiy@users.noreply.github.com>
2020-06-29 16:31:14 +0300

    Added upload functionality to flask

--- flask/app.py
+++ flask/app.py
@@ -1,35 +1,61 @@
 import sys
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from flask import render_template,Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import pandas as pd
 import os
 import numpy as np
 
-app.config['UPLOAD_FOLDER']='.\upload'
+import os
 
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+
+UPLOAD_FOLDER = './/upload'
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
+app.config['UPLOAD_FOLDER'] = "static\\files"
 
 
-@app.route('/hello',methods=['POST'])
-def hello():
-    return 'Hello, World'
+def allowed_image_filesize(filesize):
+    print(filesize)
+    if int(filesize) <= app.config["MAX_CONTENT_LENGTH"]:
+        return True
+    else:
+        return False
+@app.before_request
+def handle_chunking():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
 
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
 
+@app.route('/upload-data',methods=["GET",'POST'])
+def upload():
+    if request.method == "POST":
+        if request.files:
+            #if not allowed_image_filesize(request.cookies.get("filesize")):
+                #print("file exceeded file size")
+                #return redirect(request.url)
+
+            file = request.files["video"]
+            if file.filename == "":
+                print("Error at given name")
+                return redirect(request.url)
+
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"],secure_filename(file.filename)))
+            print("File saved")
+    return render_template("upload.html")
+with app.test_request_context():
+    print(url_for("upload"))
 if __name__ == "__main__":
-    os.mkdir("\upload")
+    if not os.path.exists("upload"):
+        os.mkdir("upload")
     try:
         port = int(sys.argv[1])
     except Exception as e:
         port = 5500
-    app.run(host='0.0.0.0',port=port,debug=False)
+    app.run(port=port,debug=True)
